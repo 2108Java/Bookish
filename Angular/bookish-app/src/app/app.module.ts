2f6e3a5d845ffda24d077bb6d8f8cea59e0a0641
Janet Joseph <janet.jos98@gmail.com>
2021-10-31 03:03:47 -0400

    Almost done design

--- Angular/bookish-app/src/app/app.module.ts
+++ Angular/bookish-app/src/app/app.module.ts
@@ -7,14 +7,15 @@ import { BookService } from './services/book.service';
 import { UserService } from './services/user.service';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LandPageComponent } from './land-page/land-page.component';
-<<<<<<< HEAD
+
 import { ReadListComponent } from './read-list/read-list.component';
 import { ToReadListComponent } from './to-read-list/to-read-list.component';
 import { CurrentListComponent } from './current-list/current-list.component';
 import { ProfileMenuComponent } from './profile-menu/profile-menu.component';
-=======
+
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> 471e89648e665ba0b0d5e8f0055ea95140a26c89
+import { RegisterFormComponent } from './register-form/register-form.component';
+
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { HttpClientModule } from '@angular/common/http';
     ReadListComponent,
     ToReadListComponent,
     CurrentListComponent,
-    ProfileMenuComponent
+    ProfileMenuComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
