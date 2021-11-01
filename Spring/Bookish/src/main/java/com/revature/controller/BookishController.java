fc68b039b840cffec63e4715995b40786637b32f
Lauren Smith <ldsmith16@liberty.edu>
2021-11-01 10:53:26 -0500

    cross origin

--- Spring/Bookish/src/main/java/com/revature/controller/BookishController.java
+++ Spring/Bookish/src/main/java/com/revature/controller/BookishController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +22,7 @@ import com.revature.service.BookService;
 import com.revature.service.UserService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class BookishController {
 
